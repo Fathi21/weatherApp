b209d715cf88bd608ce78dfbceb9f792703e0ddf
Sharif <fathifarhan21@hotmail.com>
2022-03-27 13:41:36 +0100

    ADD NEW FILES

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import './App.css';
+import Container from 'react-bootstrap/Container';
 import WeatherApp from './components/WeatherApp'
+import BarTop from './components/BarTop'
 
 function App() {
   return (
     <div className="App">
-      <WeatherApp/>
+      <BarTop/>
+      <Container>
+        <WeatherApp/>
+      </Container>
     </div>
   );
 }
--- src/components/WeatherApp.js
+++ src/components/WeatherApp.js
@@ -1,13 +1,26 @@
-import Button from 'react-bootstrap/Button';
-
-
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import Card from 'react-bootstrap/Card'
 function WeatherApp() {
   return (
     <div>
-        <Button variant="primary">Primary</Button>
-        <h1>
-            WeatherApp  
-        </h1>
+        <Row>
+            <Col md='4'>1 of 2</Col>
+            <Col md='8'>
+                <Card style={{ width: '18rem' }}>
+                    <Card.Body>
+                        <Card.Title>Card Title</Card.Title>
+                        <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
+                        <Card.Text>
+                        Some quick example text to build on the card title and make up the bulk of
+                        the card's content.
+                        </Card.Text>
+                        <Card.Link href="#">Card Link</Card.Link>
+                        <Card.Link href="#">Another Link</Card.Link>
+                    </Card.Body>
+                </Card>
+            </Col>
+        </Row>
     </div>
   );
 }
