84abd2f63a1cf965fce2b9892adf11ae7ed968b9
Sharif <fathifarhan21@hotmail.com>
2022-03-27 11:08:46 +0100

    first commit

---
+++ src/components/WeatherApp.js
@@ -0,0 +1,15 @@
+import Button from 'react-bootstrap/Button';
+
+
+function WeatherApp() {
+  return (
+    <div>
+        <Button variant="primary">Primary</Button>
+        <h1>
+            WeatherApp  
+        </h1>
+    </div>
+  );
+}
+
+export default WeatherApp;
