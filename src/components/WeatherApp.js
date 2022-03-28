36216783ae9d4e5ea66d1184fcefa995b4e6fa62
Sharif <fathifarhan21@hotmail.com>
2022-03-28 18:35:09 +0100

    --

--- src/components/WeatherApp.js
+++ src/components/WeatherApp.js
@@ -1,6 +1,8 @@
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 import Card from 'react-bootstrap/Card'
+
+
 function WeatherApp() {
     //https://codepen.io/derekjp/pen/yyVWOq 
   return (
@@ -9,15 +11,19 @@ function WeatherApp() {
             <Col md='4'>1 of 2</Col>
             <Col md='8'>
                 <Card style={{ width: '18rem' }}>
+                    <Card.Img variant="top" src="https://images.pexels.com/photos/33545/sunrise-phu-quoc-island-ocean.jpg?cs=srgb&dl=pexels-pixabay-33545.jpg&fm=jpg" />
+
                     <Card.Body>
-                        <Card.Title>Card Title</Card.Title>
-                        <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
+                        <Card.Title>
+                            <h1>Monday</h1>
+                        </Card.Title>
+                        
+                        <Card.Title>
+                            <span class="temp">23&deg;</span>
+                        </Card.Title>
                         <Card.Text>
-                        Some quick example text to build on the card title and make up the bulk of
-                        the card's content.
+                            <span class="high-low">15&deg;/ 28&deg;</span>
                         </Card.Text>
-                        <Card.Link href="#">Card Link</Card.Link>
-                        <Card.Link href="#">Another Link</Card.Link>
                     </Card.Body>
                 </Card>
             </Col>
